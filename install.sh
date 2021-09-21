fb53b7b492fbf328c12b3b2370cdac328b8ad4ff
Christian Herweg <39617537+christianherweg0807@users.noreply.github.com>
2021-09-21 11:30:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ DEBIAN_FRONTEND=noninteractive apt-get -t buster-backports -y install nvidia-cud
 modprobe nvidia
 sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl restart ssh.service
+wget -O /tmp/hashcat.7z https://github.com/hashcat/hashcat/releases/download/v6.2.4/hashcat-6.2.4.7z
+7zr x /tmp/hashcat.7z
+ln -s hashcat-* hashcat
