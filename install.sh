c8399db55dd0602ad91381f4d057a120594c8ba1
Christian Herweg <39617537+christianherweg0807@users.noreply.github.com>
2021-09-21 11:13:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 cd /root
 sed -i 's/ main/ main contrib non-free/' /etc/apt/sources.list
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y install p7zip wget
-tmux linux-headers-$(uname -r)
-DEBIAN_FRONTEND=noninteractive apt-get -t buster-backports -y
-install nvidia-cuda-dev nvidia-cuda-toolkit nvidia-driver
+DEBIAN_FRONTEND=noninteractive apt-get -y install p7zip wget tmux linux-headers-$(uname -r)
+DEBIAN_FRONTEND=noninteractive apt-get -t buster-backports -y install nvidia-cuda-dev nvidia-cuda-toolkit nvidia-driver
 modprobe nvidia
 sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
 systemctl restart ssh.service
